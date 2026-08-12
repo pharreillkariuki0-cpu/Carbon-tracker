5f2179373cd4ef05ea1a8a5cdadff6f8e2030720
pharreillkariuki0-cpu <pharreillkariuki0@gmail.com>
2026-08-12 17:08:32 +0300

    Use environment variable for API URL

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,30 @@
+// import axios from 'axios';
+
+// const API_BASE = 'http://localhost:8000/api';
+
+// const api = axios.create({
+//     baseURL: API_BASE,
+//     headers: {
+//         'Content-Type': 'application/json',
+//     }
+// });
+
+// api.interceptors.request.use(
+//     (config) => {
+//         const token = localStorage.getItem('access_token');
+//         if (token) {
+//             config.headers.Authorization = `Bearer ${token}`;
+//         }
+//         return config;
+//     },
+//     (error) => Promise.reject(error)
+// );
+
+// export default api;
+
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:8000/api';
+const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
 
 const api = axios.create({
     baseURL: API_BASE,
